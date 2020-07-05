512845b27882323545a9a9105c485bbdd280553e
emipa606 <emil.offermo@litet.net>
2020-07-05 23:03:04 +0200

    Added support for Floors Are Light and Foundations-mod if used together, thanks Lurmey

--- Source/SimplyMoreBridges/Harmony_GenConstruct_CanPlaceBlueprintAt.cs
+++ Source/SimplyMoreBridges/Harmony_GenConstruct_CanPlaceBlueprintAt.cs
@@ -1,27 +1,34 @@
-﻿using System;
-using HarmonyLib;
+﻿using HarmonyLib;
 using RimWorld;
 using Verse;
+using System.Linq;
 
 namespace SimplyMoreBridges
 {
-	// Token: 0x02000007 RID: 7
-	[HarmonyPatch("CanPlaceBlueprintAt")]
-	[HarmonyPatch(typeof(GenConstruct))]
-	public class Harmony_GenConstruct_CanPlaceBlueprintAt
-	{
-		// Token: 0x0600000E RID: 14 RVA: 0x0000271C File Offset: 0x0000091C
-		public static void Postfix(ref AcceptanceReport __result, BuildableDef entDef, IntVec3 center, Map map)
-		{
-			TerrainDef terrainDef = entDef as TerrainDef;
-			if (terrainDef != null)
-			{
-				TerrainAffordanceDef terrainAffordanceNeeded = map.terrainGrid.TerrainAt(center).terrainAffordanceNeeded;
-				if (terrainAffordanceNeeded == TerrainAffordanceDefOf.Bridgeable || terrainAffordanceNeeded == TerrainAffordanceDefOf.BridgeableDeep)
-				{
-					__result = new AcceptanceReport(Translator.Translate("NoFloorsOnBridges"));
-				}
-			}
-		}
-	}
+    // Token: 0x02000007 RID: 7
+    [HarmonyPatch("CanPlaceBlueprintAt")]
+    [HarmonyPatch(typeof(GenConstruct))]
+    public class Harmony_GenConstruct_CanPlaceBlueprintAt
+    {
+        // Token: 0x0600000E RID: 14 RVA: 0x0000271C File Offset: 0x0000091C
+        public static void Postfix(ref AcceptanceReport __result, BuildableDef entDef, IntVec3 center, Map map)
+        {
+            var centerDef = map.terrainGrid.TerrainAt(center);
+            if (!(entDef is TerrainDef) || entDef.HasModExtension<SimplyMoreBridgesModExt>() || centerDef.HasModExtension<SimplyMoreBridgesModExt>())
+            {
+                return;
+            }
+            var terrainAffordanceNeeded = centerDef.terrainAffordanceNeeded;
+            var terrainAffordanceBridgeableDeep = DefDatabase<TerrainAffordanceDef>.AllDefs.FirstOrDefault(x => x.defName == "BridgeableDeep");
+            if (terrainAffordanceBridgeableDeep == null && terrainAffordanceNeeded == TerrainAffordanceDefOf.Bridgeable)
+            {
+                __result = new AcceptanceReport("NoFloorsOnBridges".Translate());
+            }
+            if (terrainAffordanceBridgeableDeep != null &&
+                (terrainAffordanceNeeded == TerrainAffordanceDefOf.Bridgeable || terrainAffordanceNeeded == terrainAffordanceBridgeableDeep))
+            {
+                __result = new AcceptanceReport("NoFloorsOnBridges".Translate());
+            }
+        }
+    }
 }
---
+++ Source/SimplyMoreBridges/SimplyMoreBridgesModExt.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+
+namespace SimplyMoreBridges
+{
+    class SimplyMoreBridgesModExt : DefModExtension
+    {
+        // wood or concrete 
+        public string foundationType = "wood";
+        // Diggable or Bridgeable
+        public string terrainAffordanceDefName = "Bridgeable";
+        // Gravel or Marsh
+        public string defaultTerrainDefName = "Marsh";
+    }
+}
